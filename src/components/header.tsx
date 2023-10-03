a6f1f620269bee47a6223e4328a8e21ee6deda9c
Victor Gianelli <victor.o.gianelli@gmail.com>
2023-10-03 16:51:28 -0300

    Separando componentes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
 export function Header() {
   return (
-    
+    <div className="flex flex-col gap-1">
+        <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
+        <span className="texu-sm text-zinc-400">Módulo "Desenvolvendo o Redux"</span>
+    </div>
   )
 }
